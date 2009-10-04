f26316e1b1b2d020069fffd6b700e2e4d3051cb6
Abhishek Mishra <ideamonk@gmail.com>
2009-10-04 19:39:51 +0530

    looking for answers, entry not functioning properly

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p /usr/share/gtweetbar
+cp -R ./ui /usr/share/gtweetbar/
+cp -R ./python_twitter /usr/share/gtweetbar/
+cp README /usr/share/gtweetbar/
+
+cp tweetbar.py /usr/local/bin/
+chmod u+x /usr/local/bin/tweetbar.py
+chmod g+x /usr/local/bin/tweetbar.py
+chmod a+x /usr/local/bin/tweetbar.py
+cp tweetbar.server /usr/lib/bonobo/servers/
+
+echo "Done!"
+
