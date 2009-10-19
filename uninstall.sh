88efa39ede321ca017ade02257f5b042c6c57c13
Abhishek Mishra <ideamonk@gmail.com>
2009-10-19 20:29:18 +0530

    more verbose uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,24 @@
-# GTweetBar Uninstaller Script
-# 
-# usage
-# $ sudo uninstall.sh
+#!/bin/sh
+## Installation Script for gtweetbar
+ 
+# ======================================================================
+# Copyright (C) 2009 Abhishek Mishra <ideamonk@gmail.com>
+# Time-stamp: Sun Oct 04 12:11:57 IST 2009
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+# ======================================================================
+
+# i don't know no packaging yet
+
+echo 'GTweetBar 1.0 Uninstaller'
+echo ' *** you must be a super user to be able to remove *** '
+echo ' '
+
+echo "Removing gTweetBar..."
 
 rm  /usr/local/bin/gtweetbar.py
 rm -Rf /usr/share/gtweetbar
 rm /usr/lib/bonobo/servers/gtweetbar.server
 
+echo "Done!"
